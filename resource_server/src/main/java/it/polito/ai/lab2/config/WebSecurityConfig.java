62de43aec88f5a88533826e2e7be23d9ec957f0a
Riccardo Marchi <riccardo.marchi4@gmail.com>
2020-09-08 16:22:50 +0200

    fix minor change

--- resource_server/src/main/java/it/polito/ai/lab2/config/WebSecurityConfig.java
+++ resource_server/src/main/java/it/polito/ai/lab2/config/WebSecurityConfig.java
@@ -49,7 +49,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/API/authenticate").permitAll()
+                .antMatchers("/API/authentication/**").permitAll()
                 .antMatchers("/API/notification/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
