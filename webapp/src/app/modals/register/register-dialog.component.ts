87f424379f2742c920bf1b1ecb553f1b88dc4a1a
Francesco Pavan <pavanfrancesco87@gmail.com>
2020-10-17 12:42:40 +0200

    Started two passwords confirm in register dialog

--- webapp/src/app/modals/register/register-dialog.component.ts
+++ webapp/src/app/modals/register/register-dialog.component.ts
@@ -1,34 +1,70 @@
-import {Component, OnInit} from '@angular/core';
-import {MatDialogRef} from '@angular/material/dialog';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {AuthService} from 'src/app/services/auth.service';
-import {first} from 'rxjs/operators';
-import {FileInput} from 'ngx-material-file-input';
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+  FormGroup,
+  FormBuilder,
+  AbstractControl,
+} from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { first } from 'rxjs/operators';
+import { FileInput } from 'ngx-material-file-input';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    const invalidCtrl = !!(control && control.invalid && control.parent.dirty);
+    const invalidParent = !!(
+      control &&
+      control.parent &&
+      control.parent.invalid &&
+      control.parent.dirty
+    );
+
+    return invalidCtrl || invalidParent;
+  }
+}
 
 @Component({
   selector: 'app-register-dialog',
   templateUrl: './register-dialog.component.html',
-  styleUrls: ['./register-dialog.component.css']
+  styleUrls: ['./register-dialog.component.css'],
 })
 export class RegisterDialogComponent implements OnInit {
-
   public form: FormGroup;
   public registerInvalid = false;
+  matcher = new MyErrorStateMatcher();
 
-  constructor(private fb: FormBuilder,
-              private authService: AuthService,
-              public dialogRef: MatDialogRef<RegisterDialogComponent>) {
-  }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    public dialogRef: MatDialogRef<RegisterDialogComponent>
+  ) {}
 
   ngOnInit(): void {
-    this.form = this.fb.group({
-      name: ['', Validators.pattern('^[A-Za-z]{1,32}$')],
-      surname: ['', Validators.pattern('^[A-Za-z]{1,32}$')],
-      email: ['', Validators.email],
-      id: ['', Validators.pattern('^(a|s|d)[0-9]+$')],
-      pic: [''],
-      password: ['', Validators.pattern('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$')]
-    });
+    this.form = this.fb.group(
+      {
+        name: ['', Validators.pattern('^[A-Za-z]{1,32}$')],
+        surname: ['', Validators.pattern('^[A-Za-z]{1,32}$')],
+        email: ['', Validators.email],
+        id: ['', Validators.pattern('^(a|s|d)[0-9]+$')],
+        pic: [''],
+        password: [
+          '',
+          Validators.pattern(
+            '^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$'
+          ),
+        ],
+        passwordConfirm: [''],
+      },
+      { validator: this.checkPasswords }
+    );
   }
 
   onSubmit() {
@@ -44,11 +80,19 @@ export class RegisterDialogComponent implements OnInit {
     const fileInput: FileInput = this.form.get('pic').value;
     formData.append('photo', fileInput.files[0]);
 
-    this.authService.register(formData)
-        .pipe(first())
-        .subscribe(
-            () => this.dialogRef.close(true),
-            () => this.registerInvalid = true
-        );
+    this.authService
+      .register(formData)
+      .pipe(first())
+      .subscribe(
+        () => this.dialogRef.close(true),
+        () => (this.registerInvalid = true)
+      );
+  }
+
+  checkPasswords(group: FormGroup) {
+    const pass = group.controls.password.value;
+    const confirmPass = group.controls.passwordConfirm.value;
+
+    return pass === confirmPass ? null : { notSame: true };
   }
 }
