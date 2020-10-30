e9ded9236baaddad4fb1155ba3f9f0141b2e195c
Riccardo Marchi <riccardo.marchi4@gmail.com>
2020-10-30 01:45:09 +0100

    add filter to assignment solution table
    Signed-off-by: Riccardo Marchi <riccardo.marchi4@gmail.com>

--- webapp/src/app/professor/assignments/tab-assignments.component.ts
+++ webapp/src/app/professor/assignments/tab-assignments.component.ts
@@ -11,6 +11,7 @@ import {DomSanitizer} from '@angular/platform-browser';
 import {AssignmentService} from '../../services/assignment.service';
 import {animate, state, style, transition, trigger} from '@angular/animations';
 import {AssignmentSolutionDetails} from '../../models/assignment-solution-details.model';
+import {AssignmentStatus} from '../../models/assignment-solution.model';
 
 /**
  * AssignmentsComponent
@@ -41,6 +42,8 @@ export class TabProfessorAssignmentsComponent implements AfterViewInit {
     this.dataSource.data = assignments;
   }
   expandedElement: Assignment | null;
+  assignmentStatuses = Object.values(AssignmentStatus).filter(isNaN);
+  filteredStatuses: string[] = [];
 
   constructor(public dialog: MatDialog,
               private assignmentService: AssignmentService,
@@ -76,6 +79,11 @@ export class TabProfessorAssignmentsComponent implements AfterViewInit {
 
   dateString(statusTs: string): string {
     const date = new Date(statusTs);
-    return date.toLocaleTimeString() + ' - ' + date.toLocaleDateString();
+    return date.toLocaleDateString() + ' at ' + date.toLocaleTimeString();
+  }
+
+  changeFiltering(status: string) {
+    this.filteredStatuses.includes(status) ?
+        this.filteredStatuses = this.filteredStatuses.filter(s => s !== status) : this.filteredStatuses.push(status);
   }
 }
